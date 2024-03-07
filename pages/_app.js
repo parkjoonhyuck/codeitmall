42659b6d36c6559ff9b9f4a59fe889686f600dd1
parkjoonhyuck <dark5062@naver.com>
2024-03-08 04:31:15 +0900

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import "@/styles/globals.css";
+import Container from '@/components/Container';
+import Headers from '@/components/Header';
+import { ThemeProvider } from '@/lib/ThemeContext';
+import '@/styles/global.css';
+import Head from 'next/head';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>CodeitMall</title>
+        <link rel="icon" href="/favicon.ico"></link>
+      </Head>
+      <ThemeProvider>
+        <Headers></Headers>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
